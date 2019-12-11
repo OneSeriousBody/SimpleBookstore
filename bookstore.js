2a86646dda906b2b5a967460290fff619f732c7c
OneSeriousBody <fadeprogramerwzt@163.com>
2019-12-11 19:06:42 +0800

    second commit

--- bookstore.js
+++ bookstore.js
@@ -2,7 +2,7 @@
 		4种分类
 	*/
 	function change1(){
-			document.getElementById("book1").style.display = "inline";
+			/*document.getElementById("book1").style.display = "inline";
 			document.getElementById("book2").style.display = "inline";
 			document.getElementById("book3").style.display = "inline";
 			document.getElementById("book4").style.display = "inline";
@@ -10,7 +10,11 @@
 			document.getElementById("book6").style.display = "inline";
 			document.getElementById("book7").style.display = "inline";
 			document.getElementById("book8").style.display = "inline";
-			document.getElementById("book9").style.display = "inline";
+			document.getElementById("book9").style.display = "inline";*/
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				document.getElementById(book).style.display = "inline";
+			}
 			document.getElementById("h11").innerHTML = "全部 ";
 			document.getElementById("span1").innerHTML = "共9种商品";
 			document.getElementById("span2").innerHTML = "首页 > 全部分类 ";
@@ -44,7 +48,7 @@
 			document.getElementById("span2").innerHTML = "首页 > JavaEE分类 ";
 	}
 	function change4(){
-			document.getElementById("book1").style.display = "none";
+			/*document.getElementById("book1").style.display = "none";
 			document.getElementById("book2").style.display = "none";
 			document.getElementById("book3").style.display = "none";
 			document.getElementById("book4").style.display = "none";
@@ -52,7 +56,15 @@
 			document.getElementById("book6").style.display = "none";
 			document.getElementById("book7").style.display = "none";
 			document.getElementById("book8").style.display = "inline";
-			document.getElementById("book9").style.display = "none";
+			document.getElementById("book9").style.display = "none";*/
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i != 8){
+					document.getElementById(book).style.display = "none";
+				}else{
+					document.getElementById(book).style.display = "inline";
+				}
+			}
 			document.getElementById("h11").innerHTML = "JavaScript ";
 			document.getElementById("span1").innerHTML = "共1种商品";
 			document.getElementById("span2").innerHTML = "首页 > JavaScript分类 ";
@@ -60,14 +72,14 @@
 	
 	function getCookie(c_name){				/* 获取Cookie */
 		if (document.cookie.length > 0){
-	         c_start=document.cookie.indexOf(c_name + "=");
-			 if(c_start!=-1){
-				c_start=c_start + c_name.length+1;
-				c_end=document.cookie.indexOf(";",c_start);
-				if (c_end==-1){ 
-					c_end=document.cookie.length;
+	         c_start = document.cookie.indexOf(c_name + "=");	/* 取得这个cookie名在整个cookie中的位置 */
+			 if(c_start != -1){			/* =-1即没有找到这个cookie */
+				c_start = c_start + c_name.length + 1; /* 把位置确定在这个cookie名的“=”符号的后面 */
+				c_end = document.cookie.indexOf(";",c_start);	/* 从“=”的位置开始查找的，直到";"的位置 */
+				if (c_end == -1){	
+					c_end = document.cookie.length;	/* 找到“;”前的位置下标 */
 				}
-				return document.cookie.substring(c_start,c_end);
+				return document.cookie.substring(c_start,c_end);	/* 取该cookie名的等号后面到分号前面的字符串，即对应的cookie的值 */
 			}
 		}
 		return "";
@@ -88,7 +100,7 @@
 		username = getCookie(c_name);
 		if (username != null && username != ""){
 			document.getElementById("login").innerHTML = "user";
-			alert("您已登录");
+			alert("尊敬的客户，欢迎您的使用!");
 			return true;
 		}else{
 			alert("您还未登录，请先登录！");
@@ -97,10 +109,22 @@
 		}
 	}
 
-	function checkCookie3(c_name,book_num){     /* 添加书籍到购物车 */
+	function checkCookie22(){
+		username = getCookie('username');
+		if (username != null && username != ""){
+			document.getElementById("login").innerHTML = "user";
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	function checkCookie3(c_name,book_num,book_count_num,book_count){     /* 添加书籍到购物车 */
 		username = getCookie(c_name);
 		if (username != null && username != ""){
 			setCookie(book_num,"1",1);
+			setCookie(book_count_num,book_count,1);
+			alert("商品已添加至购物车");
 		}else{
 			alert("您还未登录，请先登录！");
 			window.open("login.html","_self");
@@ -112,5 +136,127 @@
 		var exdate = new Date();
 		exdate.setDate(exdate.getDate()+expiredays); 
 		document.cookie = c_name+ "=" + escape(value) + ((expiredays == null) ? "" : ";expires="+exdate.toGMTString());    /* escape()对字符串进行编码 */
-		alert("商品已添加至购物车");
 	}
+
+	function searchbook(){	/* 书籍搜索 */
+		var text = document.getElementById("searchtext");
+		if(text.value == "Java就业培训教程"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 1){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaSE分类  ";
+		}
+		if(text.value == "精通Hibernate"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 2){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaEE分类 ";
+		}
+		if(text.value == "Head First Java"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 3){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaSE分类 ";
+		}
+		if(text.value == "Java编程思想"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 4){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaSE分类  ";
+		}
+		if(text.value == "Spring企业开发"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 5){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaEE分类  ";
+		}
+		if(text.value == "Java核心技术"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 6){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaSE分类  ";
+		}
+		if(text.value == "Struts2深入理解"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 7){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaEE分类  ";
+		}
+		if(text.value == "JavaScript"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 8){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaScript分类  ";
+		}
+		if(text.value == "JavaWeb开发"){
+			for(i=1; i<10; i++){
+				var book = "book" + i;
+				if(i == 9){
+					document.getElementById(book).style.display = "inline";
+				}else{
+					document.getElementById(book).style.display = "none";
+				}
+			}
+			document.getElementById("h11").innerHTML = "全部 ";
+			document.getElementById("span1").innerHTML = "共1种商品";
+			document.getElementById("span2").innerHTML = "首页 > JavaEE分类  ";
+		}
+	}
+
+	window.onload = checkCookie22;
--- cart.js
+++ cart.js
@@ -1,18 +1,20 @@
-	function getCookie(c_name){
+	var sumend = "";
+	
+	function getCookie(c_name){				/* 获取Cookie */
 		if (document.cookie.length > 0){
-	         c_start=document.cookie.indexOf(c_name + "=");
-			 if(c_start!=-1){
-				c_start=c_start + c_name.length+1;
-				c_end=document.cookie.indexOf(";",c_start);
-				if (c_end==-1){ 
-					c_end=document.cookie.length;
+	         c_start = document.cookie.indexOf(c_name + "=");	/* 取得这个cookie名在整个cookie中的位置 */
+			 if(c_start != -1){			/* =-1即没有找到这个cookie */
+				c_start = c_start + c_name.length + 1; /* 把位置确定在这个cookie名的“=”符号的后面 */
+				c_end = document.cookie.indexOf(";",c_start);	/* 从“=”的位置开始查找的，直到";"的位置 */
+				if (c_end == -1){	
+					c_end = document.cookie.length;	/* 找到“;”前的位置下标 */
 				}
-				return document.cookie.substring(c_start,c_end);
+				return document.cookie.substring(c_start,c_end);	/* 取该cookie名的等号后面到分号前面的字符串，即对应的cookie的值 */
 			}
 		}
 		return "";
 	}
-	
+
 	function add(){
 		var book1 = getCookie("book1");
 		var book2 = getCookie("book2");
@@ -23,92 +25,151 @@
 		var book7 = getCookie("book7");
 		var book8 = getCookie("book8");
 		var book9 = getCookie("book9");
+		var sum = 0;
 
 		if(book1 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count1');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/8758723-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>Java就业培训教程</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>28</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*28 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book1\")'></td>";
 			tab.appendChild(tr);
+			sum += count*28;
 		}
 		if(book2 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count2');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/8991366-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>精通Hibernate</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>38</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*38 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book2\")'></td>";
 			tab.appendChild(tr);
+			sum += count*38;
 		}
 		if(book3 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count3');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/9265169-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>Head First Java</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>40</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*40 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book3\")'></td>";
 			tab.appendChild(tr);
+			sum += count*40;
 		}
 		if(book4 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count4');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/9317290-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>Java编程思想</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>35</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*35 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book4\")'></td>";
 			tab.appendChild(tr);
+			sum += count*35;
 		}
 		if(book5 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count5');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/20029394-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>Spring企业开发</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>56</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*56 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book5\")'></td>";
 			tab.appendChild(tr);
+			sum += count*56;
 		}
 		if(book6 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count6');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/20285763-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>Java核心技术</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>55</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*55 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book6\")'></td>";
 			tab.appendChild(tr);
+			sum += count*55;
 		}
 		if(book7 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count7');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/20385925-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>Struts2深入理解</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>40</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*40 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book7\")'></td>";
 			tab.appendChild(tr);
+			sum += count*40;
 		}
 		if(book8 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count8');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/22722790-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>JavaScript</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>51</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*51 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book8\")'></td>";
 			tab.appendChild(tr);
+			sum += count*51;
 		}
 		if(book9 == "1"){
 			var tab = document.getElementById("tab");
 			var tr = document.createElement("tr");
+			var count = getCookie('count9');
 			tr.innerHTML = "<td colspan=3 align=center><div class='bookimg'><img src='book_img/22788412-1_l.jpg' border='0'/></div></td>"
 							+ "<td colspan=3 align=center><font color=blue>JavaWeb开发</font></td>"
 							+ "<td colspan=2 align=center><font color=blue>70</font></td>"
-							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this)'></td>";
+							+ "<td colspan=2 align=center><font color=blue>" + count + "</font></td>"
+							+ "<td colspan=2 align=center><font color=blue>" + count*70 + "</font></td>"
+							+ "<td colspan=1 align=center><input type='button' value='移除' onclick='del(this,\"book9\")'></td>";
 			tab.appendChild(tr);
+			sum += count*70;
 		}
+		document.getElementById("total").innerHTML = sum;
+		sumend = sum;
 	}
 
-	function del(thisdel){		/* 订单删除 */
+	function del(thisdel,c_name){		/* 订单删除 */
 		thisdel.parentNode.parentNode.parentNode.removeChild(thisdel.parentNode.parentNode);
+		delCookie(c_name);
+	}
+
+	function delCookie(name){
+		var exp = new Date();
+		exp.setTime(exp.getTime() - 1);
+		var cval=getCookie(name);
+		if(cval!=null)
+		document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+	}
+
+	function pay(){
+		var r=confirm("共计" + sumend + "元，您确定去支付？");
+		if (r==true){
+			alert("正在为您跳转支付页面，请点击确定！");
+			window.open("https://www.taobao.com/");
+		}else{
+			alert("亲，您要抛弃我了吗？？？");
+		}
+		return true;
 	}
 
 	window.onload = add;
